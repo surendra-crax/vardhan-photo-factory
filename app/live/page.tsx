f453ef3472cad7bb987be93c8a763f074e9efa1f
Bandaru Surendra <118333138+surendra-crax@users.noreply.github.com>
2026-04-03 12:51:53 +0530

    Update page.tsx

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -9,7 +9,7 @@ type EventType = {
   date: string;
   location: string;
   thumbnail: string;
-  youtubeLiveUrl: string;
+  // youtubeLiveUrl: string;
   photoUrl: string;
 };
 
@@ -18,7 +18,7 @@ export default function LivePage() {
 
   const events: EventType[] = [
     {
-      id: 4,
+      id: 5,
       name: "HARSHA WEDS RAMYA || 25-2-2026 ||",
       description:
         "Experience the ceremony live with cinematic coverage and premium streaming.",
@@ -30,7 +30,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/N8I6susAA2TCMhcMiEqh",
     },
     {
-      id: 3,
+      id: 4,
       name: "Yaswanth Weds Jyothisri || 4-3-2026 ||",
       description:
         "Experience the ceremony live with cinematic coverage and premium streaming.",
@@ -41,7 +41,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/3btTWwEEV9msgOFnlbCh",
     },
     {
-      id: 2,
+      id: 3,
       name: "Kesava Weds Sri Vidya || 7-3-2026 ||",
       description:
         "Experience the ceremony live with cinematic coverage and premium streaming.",
@@ -52,7 +52,7 @@ export default function LivePage() {
       photoUrl: "#",
     },
     {
-      id: 1,
+      id: 2,
       name: "Pranavi Weds Chandra sekhar || 11-3-2026 ||",
       description:
         "Experience the ceremony live with cinematic coverage and premium streaming.",
@@ -61,6 +61,17 @@ export default function LivePage() {
       thumbnail: "/thumbnail/march 11.jpg",
       youtubeLiveUrl: "https://youtube.com/live/V1Dd26uBbMo?feature=share",
       photoUrl: "https://link.camtom.in/1FqUgopWjyEQ88V4jiZ7",
+    },
+    {
+      id: 1,
+      name: "Surya weds Deepti || 03-4-2026 ||",
+      description:
+        "Experience the ceremony with live photos.",
+      date: "2026-03-11",
+      location: "Tanuku, Andhrapradesh",
+      thumbnail: "/thumbnail/april 3.jpg",
+      // youtubeLiveUrl: "https://youtube.com/live/V1Dd26uBbMo?feature=share",
+      photoUrl: "https://link.camtom.in/dkBMfSS8hkbXoSXoz7wb",
     },
   ];
 
@@ -119,14 +130,14 @@ export default function LivePage() {
 
               {/* RIGHT DETAILS */}
               <div className="lg:col-span-2 space-y-6">
-                <a
+                {/* <a
                   href={event.youtubeLiveUrl}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="relative block w-full overflow-hidden rounded-2xl py-4 text-lg font-semibold text-white text-center backdrop-blur-xl border border-white/20 bg-gradient-to-b from-red-500/80 to-red-700/80 shadow-[0_10px_40px_rgba(255,0,0,0.5)] transition hover:scale-[1.03] active:scale-[0.97]"
                 >
                   <span className="relative z-10">🔴 Watch Live</span>
-                </a>
+                </a> */}
 
                 <a
                   href={event.photoUrl}
