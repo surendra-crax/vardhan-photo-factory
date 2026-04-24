a3c27dac7d694e8c41aeaa337d1df555993cbc45
Bandaru Surendra <118333138+surendra-crax@users.noreply.github.com>
2026-04-25 00:54:38 +0530

    Update LiveCenterPopup.tsx

--- components/LiveCenterPopup.tsx
+++ components/LiveCenterPopup.tsx
@@ -5,13 +5,13 @@ import { useEffect, useState } from "react";
 /* =======================
    CHANGE ONLY THESE
 ======================= */
- const LIVE_YOUTUBE_URL =
-  "https://youtube.com/live/EpzyGlHKU70";
+ // const LIVE_YOUTUBE_URL =
+ //  "https://youtube.com/live/EpzyGlHKU70";
 
 const PHOTO_URL =
-   "https://link.camtom.in/bGWhgaaZnr3BymzfOcFQ";
+   "https://link.camtom.in/YmppGqDz6LWWjJxhkDbu";
 
-const THUMBNAIL_URL = "/thumbnail/april 11.jpg";
+const THUMBNAIL_URL = "/thumbnail/april 25.jpeg";
 
 const ANIMATION_DURATION = 700;
 
@@ -82,7 +82,7 @@ const LiveCenterPopup = () => {
         </p>
 
         {/* Watch Live */}
-         <a
+         {/* <a
           href={LIVE_YOUTUBE_URL}
           target="_blank"
           rel="noopener noreferrer"
@@ -94,7 +94,7 @@ const LiveCenterPopup = () => {
           }}
         >
           🔴 Watch Live on YouTube
-        </a> 
+        </a>  */}
 
         {/* Get Photos */}
         <a
