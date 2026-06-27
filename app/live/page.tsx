f9ef00ce11c477416a9ced18818154d319b7b825
Bandaru Surendra <118333138+surendra-crax@users.noreply.github.com>
2026-06-28 01:27:07 +0530

    Update page.tsx

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -18,7 +18,7 @@ export default function LivePage() {
 
   const events: EventType[] = [
     {
-      id: 12,
+      id: 13,
       name: "HARSHA WEDS RAMYA || 25-2-2026 ||",
       description:
         "Experience the ceremony live with cinematic coverage and premium streaming.",
@@ -29,7 +29,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/N8I6susAA2TCMhcMiEqh",
     },
     {
-      id: 11,
+      id: 12,
       name: "Yaswanth Weds Jyothisri || 4-3-2026 ||",
       description:
         "Experience the ceremony live with cinematic coverage and premium streaming.",
@@ -40,7 +40,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/3btTWwEEV9msgOFnlbCh",
     },
     {
-      id: 10,
+      id: 11,
       name: "Kesava Weds Sri Vidya || 7-3-2026 ||",
       description:
         "Experience the ceremony live with cinematic coverage and premium streaming.",
@@ -51,7 +51,7 @@ export default function LivePage() {
       photoUrl: "#",
     },
     {
-      id: 9,
+      id: 10,
       name: "Pranavi Weds Chandra sekhar || 11-3-2026 ||",
       description:
         "Experience the ceremony live with cinematic coverage and premium streaming.",
@@ -62,7 +62,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/1FqUgopWjyEQ88V4jiZ7",
     },
     {
-      id: 8,
+      id: 9,
       name: "Surya weds Deepti || 03-4-2026 ||",
       description:
         "Experience the ceremony with live photos.",
@@ -73,7 +73,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/dkBMfSS8hkbXoSXoz7wb",
     },
     {
-      id: 7,
+      id: 8,
       name: "Rohit weds Sudha Rani || 11-4-2026 ||",
       description:
         "Experience the ceremony with live photos.",
@@ -84,7 +84,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/bGWhgaaZnr3BymzfOcFQ",
     },
     {
-      id: 6,
+      id: 7,
       name: "Naresh weds Hima bindu || 25-4-2026 ||",
       description:
         "Experience the ceremony with live photos.",
@@ -95,7 +95,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/YmppGqDz6LWWjJxhkDbu",
     },
     {
-      id: 5,
+      id: 6,
       name: "Sree Bhagya weds Sai || 30-4-2026 ||",
       description:
         "Experience the ceremony with live photos.",
@@ -106,7 +106,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/TJYv2kK3RCm8Cux6Zu51",
     },
     {
-      id: 4,
+      id: 5,
       name: "Vamsi Chandra weds Venkata Satya || 01-5-2026 ||",
       description:
         "Experience the ceremony with live photos.",
@@ -117,7 +117,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/tZRkdnJP35TNLh8BcgT1",
     },
     {
-      id: 3,
+      id: 4,
       name: "First birthday of Midhuna|| 02-6-2026 ||",
       description:
         "Experience the birthday party with live photos.",
@@ -128,7 +128,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/0kWNDBaOfkacnzfdNJqu",
     },
      {
-      id: 2,
+      id: 3,
       name: "DOTHI CEREMONY HASWANTH & KUSAL|| 17-6-2026 ||",
       description:
         "Experience the dothi ceremony with live photos.",
@@ -139,7 +139,7 @@ export default function LivePage() {
       photoUrl: "https://link.camtom.in/ouTX4XRbZmHo2CSY89hD",
     },
     {
-      id: 1,
+      id: 2,
       name: "ENGAGEMENT CEREMONY OF VINAY & MAHATHI|| 25-6-2026 ||",
       description:
         "Experience the dothi ceremony with live photos.",
@@ -148,6 +148,17 @@ export default function LivePage() {
       thumbnail: "/thumbnail/June 25.jpg",
       youtubeLiveUrl: "https://youtube.com/live/6TRlTlqIm7E?feature=share",
       photoUrl: "https://link.camtom.in/vLmB95TNzuetHmpj4rk4",
+    },
+    {
+      id: 1,
+      name: "ETarak Dhothi Ceremony | Harshaali Half Saree Ceremony|| 28-6-2026 ||",
+      description:
+        "Experience the ceremony with live photos.",
+      date: "2026-06-28",
+      location: "Tanuku, Andhrapradesh",
+      thumbnail: "/thumbnail/June 28.jpg",
+      youtubeLiveUrl: "https://youtube.com/live/xqVpCtqujzM?feature=share",
+      photoUrl: " https://link.camtom.in/byf6fclgksQ24x0JOye9",
     },
   ];
 
